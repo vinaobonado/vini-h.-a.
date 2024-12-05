ffd148be0cf3d5aa5f1392a44c28af41fcb700d2
vinaobonado <00001093321325sp@al.educacao.sp.gov.br>
2024-12-05 11:48:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criaCartao(
+    'jogos',
+    'qual o primeiro jogo eletronico do mundo?',
+    'tennis for two, criando em 1958'
+)
+
+criaCartao(
+    'jogos',
+    'qual o jogo ganhador do game of the year em 2018?',
+    'foi God of War '
+)
+
+criaCartao(
+    'jogos',
+    'qual o jogo mais jogado da atualidade?',
+    'Fortinite'
+)
+
+criaCartao(
+    'jogos',
+    'qual foi o ano do lancamento de the legend of zelda majora`s mask?',
+    '2000'
+)
+criaCartao(
+    'jogos',
+    'e qual o ano do antecessor de the legend of zelda majora`s mask, the legend of zelda ocarina of time?',
+    '1999'
+)
+
+criaCartao(
+    'jogos',
+    'que empresea é a criadora da steam?',
+    'A Valve'
+)
+
+criaCartao(
+    'jogos',
+    'que jogo tem uma mecanica patenteada exclusiva?',
+    'middle-earth: shadow of mordor'
+)
+
+criaCartao(
+    'jogos',
+    'que franquia consolidou a rockstar games no mundo dos jogos?',
+    'grand theft auto, mais conhecido como GTA'
+)
+criaCartao(
+    'jogos',
+    'quem é o criador do minecraft?',
+    'Markus Persson, támbem conhecido como notch'
+)
+
+criaCartao(
+    'jogos',
+    'qual a maior franquia de jogos?',
+    'suoper mario é a mais vendida, enquanto pokemon é a que obteve mais lucros'
+)
